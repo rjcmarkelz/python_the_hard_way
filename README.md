760862489211a7bf71079ddc0dddcd263929a7ce
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-05-09 15:26:27 -0700

    initial commit for exercise files 1-30. Note not all present because code was not required for each exercise.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Python-the-Hard-Way
 ===================
 
 My example code from working through the python tutorials.
+http://learnpythonthehardway.org/
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Cody Markelz"
+my_age = 28
+my_height = 70 #inches
+my_weight = 174 #lbs
+my_eyes =   'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, 
+	my_age + my_height + my_weight)
